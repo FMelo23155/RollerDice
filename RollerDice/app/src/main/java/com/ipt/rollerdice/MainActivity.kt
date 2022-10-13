c8d3d5e5725dfa3ae67ee8f72f9c8cda0864b24f
FMelo23155 <aluno23155@ipt.pt>
2022-10-13 16:30:43 +0100

    Dados inseridos
    Inserir as imagens

---
+++ RollerDice/app/src/main/java/com/ipt/rollerdice/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.ipt.rollerdice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Random
+
+
+class MainActivity : AppCompatActivity() {
+
+    // define the pointer to the ImageView
+    lateinit var diceImage:ImageView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // find our button
+        var rollDiceBt: Button =findViewById(R.id.btRollDice)
+        // define that the button will react to 'click'
+        rollDiceBt.setOnClickListener{
+            // add a message to screen when you press the button
+            Toast.makeText(this,"The button was pressed",Toast.LENGTH_SHORT).show()
+            // generate the dice value
+            rollDice()
+        }
+
+    }
+
+    /**
+     * generate the dice value
+     */
+
+    private fun rollDice() {
+        // get the dice value
+        var randomDiceValue=Random().nextInt(6) + 1 // [1, 6]
+
+        //find the textView
+        var resultText=findViewById<TextView>(R.id.resultTextView)
+
+        //change the content of TextView
+        resultText.text=randomDiceValue.toString()
+
+        // define the face of dice to show on screen
+        var drawableResource=when(randomDiceValue){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        // Assign the image to ImageView
+        //var diceImage=findViewById<ImageView>(R.id.diceimageView)
+        diceImage.setImageResource(drawableResource)
+    }
+}
